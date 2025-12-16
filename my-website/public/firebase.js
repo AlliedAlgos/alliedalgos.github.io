cecad6e059516c408cb61ccb275ecb5b1b351553
SID AG <121702864+agsid@users.noreply.github.com>
2025-12-15 22:20:08 -0500

    lol

---
+++ my-website/public/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBESpx6a7YT1qoaNZ4P-9j7X0SjAhdkePg",
+  authDomain: "alliedalgo.firebaseapp.com",
+  databaseURL: "https://alliedalgo-default-rtdb.firebaseio.com",
+  projectId: "alliedalgo",
+  storageBucket: "alliedalgo.firebasestorage.app",
+  messagingSenderId: "333625041078",
+  appId: "1:333625041078:web:b4d7d3cfc5d3053b238617"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// 🔥 Realtime Database
+const db = getDatabase(app);
+
+console.log("Realtime Database connected ✅");
+
+export { db };
