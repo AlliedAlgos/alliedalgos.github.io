39b9d27b9e5be454d7fe962c6ea7e756931533f8
SID AG <121702864+agsid@users.noreply.github.com>
2025-12-18 18:18:40 -0500

    backend_start to work

--- v2/backend/public/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBESpx6a7YT1qoaNZ4P-9j7X0SjAhdkePg",
-  authDomain: "alliedalgo.firebaseapp.com",
-  databaseURL: "https://alliedalgo-default-rtdb.firebaseio.com",
-  projectId: "alliedalgo",
-  storageBucket: "alliedalgo.firebasestorage.app",
-  messagingSenderId: "333625041078",
-  appId: "1:333625041078:web:b4d7d3cfc5d3053b238617"
-};
